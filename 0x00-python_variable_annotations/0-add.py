f06abf8fb868d79dc326fe4cf6699fa725d81445
effgrandad <effgrandad@gmail.com>
2024-10-10 15:03:12 +0000

    commit work on annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+type annotated function for summing two numbers
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''function to sum two numbers together
+    '''
+    return a + b
