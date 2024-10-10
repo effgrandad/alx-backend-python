e58132a3eab1af3b55f51e8a4e04eb0ade409bfe
effgrandad <effgrandad@gmail.com>
2024-10-10 17:46:34 +0000

    fix errors

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,20 @@ annotated module that return a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''Converting k, v to a tuple
+from typing import Tuple, Union
+
+v_type = Union[int, float]
+return_type = Tuple[str, float]
+
+
+def to_kv(k: str, v: v_type) -> return_type:
     '''
-    return (k, float(v_squared))
+    conver k, v to a tuple
+    Args:
+        k (str): any valid string value
+        v (int/float): An int or float value
+    '''
+    if not isinstance(k, str):
+        raise TypeError('expected a string')
+    v_squared: float = v * v
+    return tuple([k, v**2])
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,11 +3,19 @@
 '''
 annotated function param 
 '''
+
 from typing import Iterable, List, Sequence, Tuple
+
+lst_type = Iterable[Sequence]
+return_type = List[Tuple[Sequence, int]]
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''
-    function to access length of a list of sequences.
+    function to access length of a list of sequences
+    Args:
+        lst (list): A list of elements
+    Returns:
+        Returns a list
     '''
     return [(i, len(i)) for i in lst]
